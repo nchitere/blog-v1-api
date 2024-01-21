17b84872f9f21f68108fdb66ff927de900cc7254
nchitere <nchitere@gmail.com>
2024-01-21 09:14:21 +0300

    edited config.py file

--- instance/config.py
+++ instance/config.py
@@ -15,20 +15,3 @@ app_configuration={
 
 AppConfig = app_configuration.get('development')
 
-# blogs/22/comment == 400 blog does not exist
-# blogs/122/comment 
-# Modify post end point to add comments. default([])
-{
-    123: {
-        "id": 123,
-        "title": "recipe",
-        "author": "Herbert",
-        "comments": [{"id": 1, "content": "Nice post", "Author": "george"}],
-    },
-    122: {
-        "id":122,
-        "title": "recipe1",
-        "author": "Chitere",
-        "comments": []
-    }
-}
